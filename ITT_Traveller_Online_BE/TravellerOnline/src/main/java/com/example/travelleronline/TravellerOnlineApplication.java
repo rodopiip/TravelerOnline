3a607cb11a8dbed7b8cbc20abd37d93d94ff060e
rodopiip <mariaradeva9749@gmail.com>
2023-04-13 16:41:36 +0300

    revised .gitignore

---
+++ ITT_Traveller_Online_BE/TravellerOnline/src/main/java/com/example/travelleronline/TravellerOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.example.travelleronline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravellerOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravellerOnlineApplication.class, args);
+    }
+
+}
---
+++ ITT_Traveller_Online_BE/TravellerOnline/src/test/java/com/example/travelleronline/TravellerOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travelleronline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravellerOnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
