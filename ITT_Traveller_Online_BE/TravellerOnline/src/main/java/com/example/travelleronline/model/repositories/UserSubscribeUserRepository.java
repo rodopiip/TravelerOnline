d61df6ebd82c77cb12623c54e725bd4f6ddf02d1
Peter Rachev <peshodimrachev@gmail.com>
2023-04-16 19:44:46 +0300

    Starting UserToUser features

--- ITT_Traveller_Online_BE/TravellerOnline/src/main/java/com/example/travelleronline/model/repositories/UserSubscribeUserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.travelleronline.model.repositories;
-
-import com.example.travelleronline.model.entities.UserSubscribeUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserSubscribeUserRepository extends JpaRepository<UserSubscribeUser,Integer> {
-}
