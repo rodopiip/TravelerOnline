b66036edfb380b2cff9c8026f7f43b3cb6b88199
rodopiip <mariaradeva9749@gmail.com>
2023-04-20 19:34:27 +0300

    db update

--- DataBase/travellerOnlineDDL.sql
+++ DataBase/travellerOnlineDDL.sql
@@ -7,11 +7,6 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema mydb
 -- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
 -- -----------------------------------------------------
 -- Schema travellerdb
 -- -----------------------------------------------------
@@ -20,7 +15,7 @@ CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
 -- Schema travellerdb
 -- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `travellerdb` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
-USE `mydb` ;
+USE `travellerdb` ;
 
 -- -----------------------------------------------------
 -- Table `travellerdb`.`categories`
@@ -28,10 +23,33 @@ USE `mydb` ;
 CREATE TABLE IF NOT EXISTS `travellerdb`.`categories` (
   `Id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NOT NULL,
-  `keywords` TEXT NULL DEFAULT NULL,
   PRIMARY KEY (`Id`))
 ENGINE = InnoDB
-AUTO_INCREMENT = 11
+AUTO_INCREMENT = 21
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `travellerdb`.`users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `travellerdb`.`users` (
+  `Id` INT NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(255) NOT NULL,
+  `last_name` VARCHAR(255) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  `email` VARCHAR(255) NOT NULL,
+  `phone_number` VARCHAR(255) NULL DEFAULT NULL,
+  `date_of_birth` DATE NOT NULL,
+  `profile_photo` VARCHAR(255) NULL DEFAULT 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+  `is_verified` BIT(1) NULL DEFAULT NULL,
+  `additional_info` VARCHAR(255) NULL DEFAULT NULL,
+  `gender` VARCHAR(255) NULL DEFAULT NULL,
+  `date_added` TIMESTAMP NOT NULL,
+  `verification_code` VARCHAR(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`Id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 6
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
@@ -48,63 +66,21 @@ CREATE TABLE IF NOT EXISTS `travellerdb`.`posts` (
   `additional_info` TEXT NULL DEFAULT NULL,
   `category_id` INT NOT NULL,
   `video_url` VARCHAR(255) NULL DEFAULT NULL,
-  `date_created` DATETIME NOT NULL DEFAULT NOW(),
+  `date_created` TIMESTAMP NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `posts_ibfk_2` (`category_id` ASC) VISIBLE,
+  INDEX `FK5lidm6cqbc7u4xhqpxm898qme` (`user_id` ASC) VISIBLE,
+  CONSTRAINT `FK5lidm6cqbc7u4xhqpxm898qme`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `travellerdb`.`users` (`Id`),
   CONSTRAINT `posts_ibfk_2`
     FOREIGN KEY (`category_id`)
     REFERENCES `travellerdb`.`categories` (`Id`))
 ENGINE = InnoDB
-AUTO_INCREMENT = 9
+AUTO_INCREMENT = 6
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
-
--- -----------------------------------------------------
--- Table `travellerdb`.`users`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `travellerdb`.`users` (
-  `Id` INT NOT NULL AUTO_INCREMENT,
-  `first_name` VARCHAR(50) NOT NULL,
-  `last_name` VARCHAR(50) NOT NULL,
-  `password` VARCHAR(100) NOT NULL,
-  `email` VARCHAR(100) NOT NULL,
-  `phone_number` VARCHAR(20) NULL DEFAULT NULL,
-  `date_of_birth` DATE NOT NULL,
-  `profile_photo` VARCHAR(255) NULL DEFAULT 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
-  `verified` TINYINT NULL DEFAULT NULL,
-  `additional_info` VARCHAR(255) NULL DEFAULT NULL,
-  `gender` VARCHAR(2) NULL DEFAULT NULL,
-  `date_added` DATETIME NOT NULL DEFAULT NOW(),
-  `verification_code` VARCHAR(100) NULL,
-  PRIMARY KEY (`Id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 11
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`posts_tag_users`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`posts_tag_users` (
-  `post_id` INT NOT NULL,
-  `tagged_user_Id` INT NOT NULL,
-  PRIMARY KEY (`post_id`, `tagged_user_Id`),
-  INDEX `fk_posts_tag_users_users1_idx` (`tagged_user_Id` ASC) VISIBLE,
-  CONSTRAINT `post_id`
-    FOREIGN KEY (`post_id`)
-    REFERENCES `travellerdb`.`posts` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `tagged_user_id`
-    FOREIGN KEY (`tagged_user_Id`)
-    REFERENCES `travellerdb`.`users` (`Id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-USE `travellerdb` ;
 
 -- -----------------------------------------------------
 -- Table `travellerdb`.`comments`
@@ -113,10 +89,10 @@ CREATE TABLE IF NOT EXISTS `travellerdb`.`comments` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `user_id` INT NOT NULL,
   `content` VARCHAR(255) NOT NULL,
-  `post_id` INT NULL,
+  `post_id` INT NULL DEFAULT NULL,
   `super_comment_id` INT NULL DEFAULT NULL,
-  `rating` INT NOT NULL DEFAULT 0,
-  `date_added` DATETIME NOT NULL DEFAULT NOW(),
+  `rating` INT NOT NULL DEFAULT '0',
+  `date_added` DATETIME(6) NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `user_id` (`user_id` ASC) VISIBLE,
   INDEX `post_id` (`post_id` ASC) VISIBLE,
@@ -131,7 +107,7 @@ CREATE TABLE IF NOT EXISTS `travellerdb`.`comments` (
     FOREIGN KEY (`super_comment_id`)
     REFERENCES `travellerdb`.`comments` (`id`))
 ENGINE = InnoDB
-AUTO_INCREMENT = 11
+AUTO_INCREMENT = 18
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
@@ -142,7 +118,7 @@ COLLATE = utf8mb4_0900_ai_ci;
 CREATE TABLE IF NOT EXISTS `travellerdb`.`images` (
   `Id` INT NOT NULL AUTO_INCREMENT,
   `post_Id` INT NOT NULL,
-  `URL` VARCHAR(255) NULL,
+  `URL` VARCHAR(255) NULL DEFAULT NULL,
   PRIMARY KEY (`Id`),
   INDEX `post_Id` (`post_Id` ASC) VISIBLE,
   CONSTRAINT `images_ibfk_1`
@@ -159,9 +135,9 @@ COLLATE = utf8mb4_0900_ai_ci;
 CREATE TABLE IF NOT EXISTS `travellerdb`.`reactions` (
   `Id` INT NOT NULL AUTO_INCREMENT,
   `user_id` INT NOT NULL,
-  `status` TINYINT NOT NULL,
-  `post_id` INT NULL,
-  `comment_id` INT NULL,
+  `status` INT NOT NULL,
+  `post_id` INT NULL DEFAULT NULL,
+  `comment_id` INT NULL DEFAULT NULL,
   PRIMARY KEY (`Id`),
   INDEX `post_id` (`post_id` ASC) VISIBLE,
   INDEX `user_id` (`user_id` ASC) VISIBLE,
@@ -176,7 +152,6 @@ CREATE TABLE IF NOT EXISTS `travellerdb`.`reactions` (
     FOREIGN KEY (`user_id`)
     REFERENCES `travellerdb`.`users` (`Id`))
 ENGINE = InnoDB
-AUTO_INCREMENT = 11
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
@@ -187,9 +162,9 @@ COLLATE = utf8mb4_0900_ai_ci;
 CREATE TABLE IF NOT EXISTS `travellerdb`.`users_save_posts` (
   `user_id` INT NOT NULL,
   `post_id` INT NOT NULL,
+  PRIMARY KEY (`user_id`, `post_id`),
   INDEX `post_id` (`post_id` ASC) VISIBLE,
   INDEX `user_id` (`user_id` ASC) VISIBLE,
-  PRIMARY KEY (`user_id`, `post_id`),
   CONSTRAINT `saved_posts_ibfk_1`
     FOREIGN KEY (`post_id`)
     REFERENCES `travellerdb`.`posts` (`id`),
@@ -197,7 +172,6 @@ CREATE TABLE IF NOT EXISTS `travellerdb`.`users_save_posts` (
     FOREIGN KEY (`user_id`)
     REFERENCES `travellerdb`.`users` (`Id`))
 ENGINE = InnoDB
-AUTO_INCREMENT = 10
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
@@ -208,9 +182,9 @@ COLLATE = utf8mb4_0900_ai_ci;
 CREATE TABLE IF NOT EXISTS `travellerdb`.`users_subscribe_to_users` (
   `subscribed_to_id` INT NOT NULL,
   `subscriber_id` INT NOT NULL,
+  PRIMARY KEY (`subscribed_to_id`, `subscriber_id`),
   INDEX `followed_id` (`subscribed_to_id` ASC) VISIBLE,
   INDEX `follower_id` (`subscriber_id` ASC) VISIBLE,
-  PRIMARY KEY (`subscribed_to_id`, `subscriber_id`),
   CONSTRAINT `subscriber_ibfk_1`
     FOREIGN KEY (`subscribed_to_id`)
     REFERENCES `travellerdb`.`users` (`Id`),
@@ -218,7 +192,6 @@ CREATE TABLE IF NOT EXISTS `travellerdb`.`users_subscribe_to_users` (
     FOREIGN KEY (`subscriber_id`)
     REFERENCES `travellerdb`.`users` (`Id`))
 ENGINE = InnoDB
-AUTO_INCREMENT = 11
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
