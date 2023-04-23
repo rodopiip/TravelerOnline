cff53d184972431956c9f41db61e58ba909fa785
rodopiip <mariaradeva9749@gmail.com>
2023-04-23 20:30:36 +0300

    database update

--- DataBase/travellerOnlineDDL.sql
+++ DataBase/travellerOnlineDDL.sql
@@ -25,7 +25,24 @@ CREATE TABLE IF NOT EXISTS `travellerdb`.`categories` (
   `name` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`Id`))
 ENGINE = InnoDB
-AUTO_INCREMENT = 21
+AUTO_INCREMENT = 24
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `travellerdb`.`comments`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `travellerdb`.`comments` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `post_id` INT NULL DEFAULT NULL,
+  `rating` INT NOT NULL,
+  `super_comment_id` INT NULL DEFAULT NULL,
+  `user_id` INT NOT NULL,
+  `date_added` DATETIME(6) NOT NULL,
+  `content` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
@@ -35,8 +52,8 @@ COLLATE = utf8mb4_0900_ai_ci;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `travellerdb`.`users` (
   `Id` INT NOT NULL AUTO_INCREMENT,
-  `first_name` VARCHAR(255) NOT NULL,
-  `last_name` VARCHAR(255) NOT NULL,
+  `first_name` VARCHAR(50) NOT NULL,
+  `last_name` VARCHAR(50) NOT NULL,
   `password` VARCHAR(255) NOT NULL,
   `email` VARCHAR(255) NOT NULL,
   `phone_number` VARCHAR(255) NULL DEFAULT NULL,
@@ -49,7 +66,7 @@ CREATE TABLE IF NOT EXISTS `travellerdb`.`users` (
   `verification_code` VARCHAR(255) NULL DEFAULT NULL,
   PRIMARY KEY (`Id`))
 ENGINE = InnoDB
-AUTO_INCREMENT = 6
+AUTO_INCREMENT = 8
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
@@ -60,54 +77,27 @@ COLLATE = utf8mb4_0900_ai_ci;
 CREATE TABLE IF NOT EXISTS `travellerdb`.`posts` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `user_id` INT NOT NULL,
-  `title` VARCHAR(255) NOT NULL,
+  `title` VARCHAR(30) NOT NULL,
   `description` TEXT NOT NULL,
   `location` VARCHAR(255) NULL DEFAULT NULL,
-  `additional_info` TEXT NULL DEFAULT NULL,
   `category_id` INT NOT NULL,
   `video_url` VARCHAR(255) NULL DEFAULT NULL,
   `date_created` TIMESTAMP NOT NULL,
+  `rating` INT NULL DEFAULT 0,
   PRIMARY KEY (`id`),
   INDEX `posts_ibfk_2` (`category_id` ASC) VISIBLE,
   INDEX `FK5lidm6cqbc7u4xhqpxm898qme` (`user_id` ASC) VISIBLE,
   CONSTRAINT `FK5lidm6cqbc7u4xhqpxm898qme`
     FOREIGN KEY (`user_id`)
     REFERENCES `travellerdb`.`users` (`Id`),
+  CONSTRAINT `FKijnwr3brs8vaosl80jg9rp7uc`
+    FOREIGN KEY (`category_id`)
+    REFERENCES `travellerdb`.`categories` (`Id`),
   CONSTRAINT `posts_ibfk_2`
     FOREIGN KEY (`category_id`)
     REFERENCES `travellerdb`.`categories` (`Id`))
 ENGINE = InnoDB
-AUTO_INCREMENT = 6
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `travellerdb`.`comments`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `travellerdb`.`comments` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `user_id` INT NOT NULL,
-  `content` VARCHAR(255) NOT NULL,
-  `post_id` INT NULL DEFAULT NULL,
-  `super_comment_id` INT NULL DEFAULT NULL,
-  `rating` INT NOT NULL DEFAULT '0',
-  `date_added` DATETIME(6) NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `user_id` (`user_id` ASC) VISIBLE,
-  INDEX `post_id` (`post_id` ASC) VISIBLE,
-  INDEX `comments_ibfk_3` (`super_comment_id` ASC) VISIBLE,
-  CONSTRAINT `comments_ibfk_1`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `travellerdb`.`users` (`Id`),
-  CONSTRAINT `comments_ibfk_2`
-    FOREIGN KEY (`post_id`)
-    REFERENCES `travellerdb`.`posts` (`id`),
-  CONSTRAINT `comments_ibfk_3`
-    FOREIGN KEY (`super_comment_id`)
-    REFERENCES `travellerdb`.`comments` (`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 18
+AUTO_INCREMENT = 14
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
@@ -116,15 +106,16 @@ COLLATE = utf8mb4_0900_ai_ci;
 -- Table `travellerdb`.`images`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `travellerdb`.`images` (
-  `Id` INT NOT NULL AUTO_INCREMENT,
-  `post_Id` INT NOT NULL,
-  `URL` VARCHAR(255) NULL DEFAULT NULL,
-  PRIMARY KEY (`Id`),
-  INDEX `post_Id` (`post_Id` ASC) VISIBLE,
-  CONSTRAINT `images_ibfk_1`
-    FOREIGN KEY (`post_Id`)
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `post_id` INT NOT NULL,
+  `url` VARCHAR(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `FKcp0pycisii8ub3q4b7x5mfpn1` (`post_id` ASC) VISIBLE,
+  CONSTRAINT `FKcp0pycisii8ub3q4b7x5mfpn1`
+    FOREIGN KEY (`post_id`)
     REFERENCES `travellerdb`.`posts` (`id`))
 ENGINE = InnoDB
+AUTO_INCREMENT = 13
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
@@ -133,22 +124,22 @@ COLLATE = utf8mb4_0900_ai_ci;
 -- Table `travellerdb`.`reactions`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `travellerdb`.`reactions` (
-  `Id` INT NOT NULL AUTO_INCREMENT,
-  `user_id` INT NOT NULL,
-  `status` INT NOT NULL,
-  `post_id` INT NULL DEFAULT NULL,
   `comment_id` INT NULL DEFAULT NULL,
-  PRIMARY KEY (`Id`),
-  INDEX `post_id` (`post_id` ASC) VISIBLE,
-  INDEX `user_id` (`user_id` ASC) VISIBLE,
-  INDEX `reactions_ibfk_2` (`comment_id` ASC) VISIBLE,
-  CONSTRAINT `reactions_ibfk_1`
-    FOREIGN KEY (`post_id`)
-    REFERENCES `travellerdb`.`posts` (`id`),
-  CONSTRAINT `reactions_ibfk_2`
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `post_id` INT NULL DEFAULT NULL,
+  `status` INT NOT NULL,
+  `user_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `FK49rifnmyo1sd243acaysemlbw` (`comment_id` ASC) VISIBLE,
+  INDEX `FKh8b4h9wybhu8tc5w11e8t3krc` (`post_id` ASC) VISIBLE,
+  INDEX `FKqmewaibcp5bxtlqxc2cawhuln` (`user_id` ASC) VISIBLE,
+  CONSTRAINT `FK49rifnmyo1sd243acaysemlbw`
     FOREIGN KEY (`comment_id`)
     REFERENCES `travellerdb`.`comments` (`id`),
-  CONSTRAINT `reactions_ibfk_3`
+  CONSTRAINT `FKh8b4h9wybhu8tc5w11e8t3krc`
+    FOREIGN KEY (`post_id`)
+    REFERENCES `travellerdb`.`posts` (`id`),
+  CONSTRAINT `FKqmewaibcp5bxtlqxc2cawhuln`
     FOREIGN KEY (`user_id`)
     REFERENCES `travellerdb`.`users` (`Id`))
 ENGINE = InnoDB
@@ -160,17 +151,16 @@ COLLATE = utf8mb4_0900_ai_ci;
 -- Table `travellerdb`.`users_save_posts`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `travellerdb`.`users_save_posts` (
-  `user_id` INT NOT NULL,
   `post_id` INT NOT NULL,
-  PRIMARY KEY (`user_id`, `post_id`),
-  INDEX `post_id` (`post_id` ASC) VISIBLE,
-  INDEX `user_id` (`user_id` ASC) VISIBLE,
-  CONSTRAINT `saved_posts_ibfk_1`
-    FOREIGN KEY (`post_id`)
-    REFERENCES `travellerdb`.`posts` (`id`),
-  CONSTRAINT `saved_posts_ibfk_2`
+  `user_id` INT NOT NULL,
+  PRIMARY KEY (`post_id`, `user_id`),
+  INDEX `FKt7p1g1dikbsmngtfd8xxaxqob` (`user_id` ASC) VISIBLE,
+  CONSTRAINT `FKt7p1g1dikbsmngtfd8xxaxqob`
     FOREIGN KEY (`user_id`)
-    REFERENCES `travellerdb`.`users` (`Id`))
+    REFERENCES `travellerdb`.`users` (`Id`),
+  CONSTRAINT `FKtmcrb0sjs62auoorcsifhsvrp`
+    FOREIGN KEY (`post_id`)
+    REFERENCES `travellerdb`.`posts` (`id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
@@ -183,13 +173,12 @@ CREATE TABLE IF NOT EXISTS `travellerdb`.`users_subscribe_to_users` (
   `subscribed_to_id` INT NOT NULL,
   `subscriber_id` INT NOT NULL,
   PRIMARY KEY (`subscribed_to_id`, `subscriber_id`),
-  INDEX `followed_id` (`subscribed_to_id` ASC) VISIBLE,
-  INDEX `follower_id` (`subscriber_id` ASC) VISIBLE,
-  CONSTRAINT `subscriber_ibfk_1`
-    FOREIGN KEY (`subscribed_to_id`)
-    REFERENCES `travellerdb`.`users` (`Id`),
-  CONSTRAINT `subscriber_ibfk_2`
+  INDEX `FKhf0akplnsfyjv308d56s54pxv` (`subscriber_id` ASC) VISIBLE,
+  CONSTRAINT `FKhf0akplnsfyjv308d56s54pxv`
     FOREIGN KEY (`subscriber_id`)
+    REFERENCES `travellerdb`.`users` (`Id`),
+  CONSTRAINT `FKr4km7vqrgap8pobyqvjcw3vbt`
+    FOREIGN KEY (`subscribed_to_id`)
     REFERENCES `travellerdb`.`users` (`Id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
